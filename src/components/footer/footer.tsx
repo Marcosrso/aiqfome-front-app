c205dfccdd749d30ae0dd7651577209864a40a73
Marcos Vinicius <marcos.rso@live.com>
2025-05-10 14:42:17 -0300

    feat:created footer component

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,13 @@
+import styles from "./footer.module.css";
+
+export default function Footer() {
+  return (
+    <footer className={styles.container}>
+      <p className={styles["signature-paragraph"]}>
+        feito com 💜 em maringá-PR
+      </p>
+      <p>aiqfome.com © 2007-2023 aiqfome LTDA.</p>
+      <span>CNPJ: 09.186.786/0001-58</span>
+    </footer>
+  );
+}
