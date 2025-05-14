70b5001f99b948eec222f2ff37834cb9f457b95f
Marcos Vinicius <marcos.rso@live.com>
2025-05-13 21:43:10 -0300

    feat: added skin and fullWidth props on Button

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,19 +1,29 @@
-"use client"
+"use client";
 import { ComponentProps } from "react";
 import classNames from "classnames";
 
 import styles from "./button.module.css";
 
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-interface ButtonProps extends ComponentProps<"button"> {}
+interface ButtonProps extends ComponentProps<"button"> {
+  skin?: "transparent" | "dark" | "primary";
+  fullWidth?: boolean;
+}
 
 export default function Button({
   children,
   className,
   type = "button",
+  skin = "transparent",
+  fullWidth = false,
   ...rest
 }: Readonly<ButtonProps>) {
-  const buttonClassName = classNames(className, styles["button-box"]);
+  const buttonClassName = classNames(
+    className,
+    styles["button-box"],
+    styles[`button-${skin}`],
+    { [styles.fullWidth]: fullWidth }
+  );
 
   return (
     <button className={buttonClassName} type={type} {...rest}>
